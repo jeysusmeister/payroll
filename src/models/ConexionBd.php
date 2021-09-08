a30c4531d6b5b380c4c78a0fdb33efb7a0c80f4a
Jeysus Farinas <jeysusmeister@gmail.com>
2021-09-08 20:30:26 +0000

    Se realizo un cambio en la configuracion del proyecto

--- src/models/ConexionBd.php
+++ src/models/ConexionBd.php
@@ -6,7 +6,7 @@
         public function conectar() {
             echo 12345;
             try {
-                $link=new PDO("pgsql:host=172.16.0.101;port=5433;dbname=prueba","postgres","s1st3m4s2424bd", 
+                $link=new PDO("pgsql:host=172.16.0.101;port=5439;dbname=prueba","postgres","s1st3m4s2424bd", 
                 [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]
             ); echo "funciona";
             } catch (PDOException $e) {

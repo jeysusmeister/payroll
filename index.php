e44c5ba194e6800c8acf6534904103e25ff4fc79
Jeysus Farinas <jeysusmeister@gmail.com>
2021-09-09 03:13:49 +0000

    Inicio del proyecto

--- index.php
+++ index.php
@@ -1,15 +1,12 @@
-<?php 
+<?php
+    //Visualización de errores
 	ini_set('display_errors', 1);
 	ini_set('display_startup_errors', 1);
 	error_reporting(E_ALL);
-
+    // end del proyecto
     require_once realpath('./vendor/autoload.php');
-    require_once realpath('./src/models/ConexionBd.php');
     require_once realpath('./config/main.php');
     use PayRoll\controllers\sites\SitesController;
     use PayRoll\models\ConexionBd;
-    /*$sites = new SitesController();
-    $sites->actionInicio(); */
-	$a = new ConexionBd();
-//    var_dump($a);
-   var_dump( $a->conectar());
+    $a = new ConexionBd();
+    $a->conectar();
--- src/models/ConexionBd.php
+++ src/models/ConexionBd.php
@@ -1,21 +1,21 @@
 <?php
-
     namespace PayRoll\models;
 	use PDO;
+
     class ConexionBd {
         public function conectar() {
-            try {
-	      $link=new \PDO("pgsql:host=172.16.0.101;port=5433;dbname=prueba;","postgres","s1st3m4s2424bd",[PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
-if ($link) {
-		echo "Connected to theb database successfully!";
-	}else{ echo "no";}
-            } catch (Exception $e) {
-               echo "ERROR ";
+        try {
+	        $link=new \PDO("pgsql:host=172.16.0.101;port=5433;dbname=prueba;","postgres","s1st3m4s2424bd",[PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
+            if ($link) {
+                echo "Connected to theb database successfully!";
+            }else{ 
+                echo "no";
             }
-
+        } catch (PDOException $e) {
+            die("[ERROR] ".$e->getMessage());
+        }
             return $link;
         }
-
 
     }
 

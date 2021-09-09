a9758da380c4c68138b486de326e183258b16bbb
Jeysus Farinas <jeysusmeister@gmail.com>
2021-09-09 03:17:24 +0000

    prueba de la conexion

--- src/models/ConexionBd.php
+++ src/models/ConexionBd.php
@@ -4,16 +4,16 @@
 
     class ConexionBd {
         public function conectar() {
-        try {
-	        $link=new \PDO("pgsql:host=172.16.0.101;port=5433;dbname=prueba;","postgres","s1st3m4s2424bd",[PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
-            if ($link) {
-                echo "Connected to theb database successfully!";
-            }else{ 
-                echo "no";
+            try {
+                $link=new \PDO("pgsql:host=172.116.0.101;port=5433;dbname=prueba;","postgres","s1st3m4s2424bd",[PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
+                if ($link) {
+                    echo "Connected to theb database successfully!";
+                }else{ 
+                    echo "no";
+                }
+            } catch (PDOException $e) {
+                die("[ERROR] ".$e->getMessage());
             }
-        } catch (PDOException $e) {
-            die("[ERROR] ".$e->getMessage());
-        }
             return $link;
         }
 

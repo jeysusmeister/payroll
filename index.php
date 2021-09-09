aee34e23c27c6dd169c480393a5f850d6149c369
Jeysus Farinas <jeysusmeister@gmail.com>
2021-09-09 07:58:24 +0000

    probando variables de conexion

--- index.php
+++ index.php
@@ -1,12 +1,12 @@
 <?php
     //Visualización de errores
-	ini_set('display_errors', 1);
-	ini_set('display_startup_errors', 1);
-	error_reporting(E_ALL);
+	//ini_set('display_errors', 1);
+	//ini_set('display_startup_errors', 1);
+	//error_reporting(E_ALL);
     // end del proyecto
     require_once realpath('./vendor/autoload.php');
     require_once realpath('./config/main.php');
     use PayRoll\controllers\sites\SitesController;
-    //use PayRoll\models\ConexionBd;
+    use PayRoll\models\patria\PatriaModel;
     $sites = new SitesController();
     $sites->actionInicio();
--- src/controllers/patria/PatriaController.php
+++ src/controllers/patria/PatriaController.php
@@ -1,12 +1,12 @@
 <?php
     namespace PayRoll\controllers\patria;
 
-    
+    use PayRoll\models\patria\PatriaModel;
     
     
     class PatriaController {
         public function obtener(string $numero):void {
-           $patriamodel = \PayRoll\models\patria\PatriaModel::obtener($numero);
+           $patriamodel=PatriaModel::obtener($numero);
            echo $patriamodel;           
         }
 

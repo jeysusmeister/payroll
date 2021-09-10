28c4c68ee5a6119b6c26f91a063abffb096a542f
Jeysus Farinas <jeysusmeister@gmail.com>
2021-09-10 14:45:47 +0000

    Realizando puebas pdo

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php
     //Visualización de errores
-	//ini_set('display_errors', 1);
-	//ini_set('display_startup_errors', 1);
-	//error_reporting(E_ALL);
+	ini_set('display_errors', 1);
+	ini_set('display_startup_errors', 1);
+	error_reporting(E_ALL);
     // end del proyecto
     require realpath('./vendor/autoload.php');
     require realpath('./config/main.php');

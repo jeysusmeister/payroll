13de713a6e49e7cdd7954b8cc7bdb91608bde6e7
Jeysus Farinas <jeysusmeister@gmail.com>
2021-09-10 16:16:32 +0000

    Probando pdo

--- src/models/patria/PatriaModel.php
+++ src/models/patria/PatriaModel.php
@@ -1,7 +1,7 @@
 <?php
 
     namespace PayRoll\models\patria;
-    require realpath('../../../vendor/autoload.php');
+    //require realpath('../../../vendor/autoload.php');
     use PayRoll\models\ConexionBd; 
     
     class PatriaModel {
